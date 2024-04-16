afcbc37121d6e75bdd585e41bfa3702be156a6ba
bibersun <bibersun@gmail.com>
2024-04-16 14:51:44 +0300

    first commit

---
+++ Bits.cs
@@ -0,0 +1,27 @@
+﻿namespace lesson_2;
+
+public class Bits<T>
+{
+    private Bits(T b)
+    {
+        if (typeof(T).ToString() == "System.Int32" || 
+            typeof(T).ToString() == "System.Byte" ||
+            typeof(T).ToString() == "System.Int64")
+        {
+            Value = b;
+        }
+    }
+
+    public T Value { get; }
+
+    //
+    public static implicit operator Bits<T>(T b)
+    {
+        return new Bits<T>(b);
+    }
+
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Реализуйте операторы неявного приведения из long,int,byt в Bits.
+using lesson_2;
+
+internal class Program
+{
+    public static void Main()
+    {
+        Bits<byte> bits = 5;
+        Console.WriteLine(bits.Value.GetType());
+        Console.WriteLine(bits);
+        
+        Bits<int> bits2 = 7;
+        Console.WriteLine(bits2.Value.GetType());
+        Console.WriteLine(bits2);
+        
+        Bits<long> bits3 = 9;
+        Console.WriteLine(bits3.Value.GetType());
+        Console.WriteLine(bits3);
+
+        bits3 = 11;
+        Console.WriteLine(bits3.Value.GetType());
+        Console.WriteLine(bits3);
+    }
+}
